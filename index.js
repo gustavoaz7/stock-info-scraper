8779b41edfa03ab8b87f93040186cc042e68ed0c
gustavoaz7 <gustavo_azevedo_7@hotmail.com>
2019-03-06 15:41:25 -0300

    Scraping all relevant stock info

--- index.js
+++ index.js
@@ -4,13 +4,36 @@ const cheerio = require('cheerio');
 const BASE_URL = 'http://www.fundamentus.com.br/detalhes.php?papel=';
 const STOCK_ID = 'CARD3';
 
+const STOCK_INFO_INDEX = {
+  [1]: 'P',
+  [13]: 'A',
+  [46]: 'LL',
+  [15]: 'LPA',
+  [14]: 'P/L',
+  [17]: 'VPA',
+  [16]: 'P/VPA',
+  [41]: 'PL',
+  [44]: 'EBIT',
+  [29]: 'ROE',
+  [31]: 'LC',
+  [37]: 'DB',
+  [23]: 'ML',
+};
+
 axios
   .get(BASE_URL + STOCK_ID)
   .then(res => res.data)
   .then(html => {
     const $ = cheerio.load(html);
-    // Cotação
-    console.log($('.data.destaque.w3').text());
+    $('td.data>span.txt').each((i, elem) => {
+      STOCK_INFO_INDEX[i] &&
+        console.log(
+          STOCK_INFO_INDEX[i],
+          $(elem)
+            .text()
+            .trim()
+        );
+    });
   })
   .catch(err => {
     //handle error
