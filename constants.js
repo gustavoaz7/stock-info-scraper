ee4b076f56f537ee7108684f5e9e7b2541aaf0f4
gustavoaz7 <gustavo_azevedo_7@hotmail.com>
2019-03-06 20:29:23 -0300

    Separate of concerns

---
+++ constants.js
@@ -0,0 +1,19 @@
+module.exports = {
+  BASE_URL: 'http://www.fundamentus.com.br/detalhes.php?papel=',
+  STOCK_INDICATORS: [
+    'P',
+    'A',
+    'LL',
+    'LPA',
+    'P/L',
+    'VPA',
+    'P/VPA',
+    'PL',
+    'EBIT',
+    'ROE',
+    'LC',
+    'ML',
+    'DB',
+  ],
+  STOCK_INDICATORS_INDEX: [1, 13, 46, 15, 14, 17, 16, 41, 44, 29, 31, 23, 37],
+};
