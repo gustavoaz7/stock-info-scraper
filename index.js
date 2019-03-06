50c85f94fe871f00769e26a0cea5698f2b5989ae
gustavoaz7 <gustavo_azevedo_7@hotmail.com>
2019-03-06 20:29:55 -0300

    Get stocks from spreadsheet & make single request

--- index.js
+++ index.js
@@ -1,41 +1,63 @@
+require('./ArrayEquals');
 const axios = require('axios');
 const cheerio = require('cheerio');
+const Excel = require('exceljs');
+const workbook = new Excel.Workbook();
+const {
+  BASE_URL,
+  STOCK_INDICATORS,
+  STOCK_INDICATORS_INDEX,
+} = require('./constants');
 
-const BASE_URL = 'http://www.fundamentus.com.br/detalhes.php?papel=';
-const STOCK_ID = 'CARD3';
+function scrapeStockInfo(stockCode) {
+  return axios
+    .get(BASE_URL + stockCode)
+    .then(res => res.data)
+    .then(html => {
+      const $ = cheerio.load(html);
+      const info = {};
 
-const STOCK_INFO_INDEX = {
-  [1]: 'P',
-  [13]: 'A',
-  [46]: 'LL',
-  [15]: 'LPA',
-  [14]: 'P/L',
-  [17]: 'VPA',
-  [16]: 'P/VPA',
-  [41]: 'PL',
-  [44]: 'EBIT',
-  [29]: 'ROE',
-  [31]: 'LC',
-  [37]: 'DB',
-  [23]: 'ML',
-};
-
-axios
-  .get(BASE_URL + STOCK_ID)
-  .then(res => res.data)
-  .then(html => {
-    const $ = cheerio.load(html);
-    $('td.data>span.txt').each((i, elem) => {
-      STOCK_INFO_INDEX[i] &&
-        console.log(
-          STOCK_INFO_INDEX[i],
-          $(elem)
+      $('td.data>span.txt').each((i, elem) => {
+        const indicator = STOCK_INDICATORS[STOCK_INDICATORS_INDEX.indexOf(i)];
+        if (indicator) {
+          info[indicator] = $(elem)
             .text()
-            .trim()
-        );
+            .trim();
+        }
+      });
+
+      return info;
+    })
+    .catch(() => {
+      console.log('Error trying to fetch stock info for:', stockCode);
+    });
+}
+
+workbook.xlsx
+  .readFile('./Stock Analysis.xlsx')
+  .then(spreadSheet => {
+    const worksheet = spreadSheet.getWorksheet(1);
+    // worksheet works in a 1-based column/row number (first element is empty)
+    let indicators = worksheet.getRow(1).values;
+    // remove first two items which are not indicators & two last which are calculated using other indicators
+    indicators = indicators.slice(3, -2);
+
+    verifyIndicators(indicators);
+
+    const stockCodes = worksheet.getColumn(1).values.slice(2);
+    console.log('stockid', stockCodes[0]);
+    scrapeStockInfo(stockCodes[1]).then(stockInfo => {
+      console.log('stockInfo', stockInfo);
     });
   })
-  .catch(err => {
-    //handle error
-    console.log('ERROR ==> ', err);
+  .catch(e => {
+    console.log(e);
   });
+
+function verifyIndicators(indicators) {
+  if (!indicators.equals(STOCK_INDICATORS)) {
+    throw new Error(
+      'Scrape indicators does not match those from spreadsheet. Please double check before proceeding'
+    );
+  }
+}
